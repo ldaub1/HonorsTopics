1433cec030b689662af4c62f779898094302361f
Lauren Daub <ldaub1@hwemail.com>
2025-09-03 22:52:08 -0700

    homework 0.2 done ._.

--- MyFileWriter.java
+++ MyFileWriter.java
@@ -1,4 +1,3 @@
-
 import java.io.*;
 import java.nio.file.*;
 import java.nio.charset.StandardCharsets;
@@ -7,44 +6,30 @@ public class MyFileWriter {
     public static void main(String[] args) {
         String data = "Hello, World!";
         String fileName1 = "example.txt";
-        String fileName2 = "example2.txt";
-        String fileName3 = "example3.txt";
-        String fileName4 = "example4.txt";
-        String fileName5 = "example5.txt";
+        String hiddenFileName2 = ".example2.txt";
+        String hiddenFolderPath = ".hidden";
+        String hiddenFolderFileName3 = "example3.txt";
 
-        // 1. Using FileWriter
-        try (FileWriter writer = new FileWriter(fileName1)) {
-            writer.write(data);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // 2. Using BufferedWriter
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName2))) {
+        // 1. Using BufferedWriter
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName1))) {
             bufferedWriter.write(data);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        // 3. Using FileOutputStream
-        try (FileOutputStream outputStream = new FileOutputStream(fileName3)) {
-            outputStream.write(data.getBytes());
+        // 2. Make Hidden File
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(hiddenFileName2))) {
+            bufferedWriter.write(data);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        // 4. Using BufferedOutputStream
-        try (BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(fileName4))) {
-            bufferedOutputStream.write(data.getBytes());
+        // 3. Make Visible File In Hidden Folder
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(new File(hiddenFolderPath, hiddenFolderFileName3)))) {
+            bufferedWriter.write(data);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        // 5. Using Files (java.nio.file)
-        try {
-            Files.write(Paths.get(fileName5), data.getBytes(StandardCharsets.UTF_8));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
