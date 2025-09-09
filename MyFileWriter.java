840a639edc35e325abe5b2b69472b951a8a3864d
Lauren Daub <ldaub1@hwemail.com>
2025-09-09 08:54:57 -0700

    about me file writer lesson

--- MyFileWriter.java
+++ MyFileWriter.java
@@ -4,28 +4,11 @@ import java.nio.charset.StandardCharsets;
 
 public class MyFileWriter {
     public static void main(String[] args) {
-        String data = "Hello, World!";
-        String fileName1 = "example.txt";
-        String hiddenFileName2 = ".example2.txt";
-        String hiddenFolderPath = ".hidden";
-        String hiddenFolderFileName3 = "example3.txt";
+        String data = "My first name is lauren.\nMy middle name is madision.\nMy last name is daub.";
+        String fileName1 = "aboutMe.txt";
 
         // 1. Using BufferedWriter
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName1))) {
-            bufferedWriter.write(data);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // 2. Make Hidden File
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(hiddenFileName2))) {
-            bufferedWriter.write(data);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // 3. Make Visible File In Hidden Folder
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(new File(hiddenFolderPath, hiddenFolderFileName3)))) {
             bufferedWriter.write(data);
         } catch (IOException e) {
             e.printStackTrace();
